85c12ebe5138a07778030d5e5c5ddd36dd0f03c4
francesca-capuzzo <f.capuzzo9966@gmail.com>
2022-02-18 13:10:55 +0100

    bot

---
+++ baseBot.js
@@ -0,0 +1,30 @@
+//1- silenzio
+//0- tradimento
+
+//play - addPoints - newGame
+
+//1 - 1 entrambi prendono 3 punti 
+//0 - 0 entrambi prendono 1 punto
+//1 - 0 tradito 5 punti silenzio 0 punti
+//0 - 1 silenzio 0 punti tradito 5 punti 
+
+class BaseBot{
+
+    constructor(){
+        let _points = 0;
+    }
+
+    play(){
+        return 1;
+    }
+
+    newGame(){
+
+    }
+
+    memorize(number){
+
+    }
+
+
+}
